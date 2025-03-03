c1091e6bfd659effc464977a7742cfdbd27f141a
George Hlongwane <georgehlongwane8@gmail.com>
2025-03-03 14:20:06 +0200

    Created project and app URLs

---
+++ main/website/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('shop/', views.shop, name='shop'),
+    path('thank_you', views.thank_you, name='thank_you'),   
+]
+
