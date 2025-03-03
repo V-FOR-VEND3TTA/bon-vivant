58e0664fc5310f4db39b4d59eef46e7c8dd8c0c3
George Hlongwane <georgehlongwane8@gmail.com>
2025-03-03 14:11:52 +0200

    Created basic views to show us the contents of each of our main templates

--- main/website/views.py
+++ main/website/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'website/index.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def cart(request):
+    return render(request, 'website/cart.html')
+
+def checkout(request):
+    return render(request, 'website/checkout.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
+
+def shop(request):
+    return render(request, 'website/shop.html')
+
+def thank_you(request):
+    return render(request, 'website/thankyou.html')
